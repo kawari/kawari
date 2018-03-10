46b2468d04fa078c5ffae6bb164c3409af71b841
Narazaka <info@narazaka.net>
2018-03-11 04:30:53 +0900

    wasm

--- build/src/em-post.js
+++ build/src/em-post.js
@@ -1,9 +1,2 @@
-  this['Module'] = Module;
-  this['FS'] = FS;
-  this['PATH'] = PATH;
-  this['ERRNO_CODES'] = ERRNO_CODES;
-  this['NODEFS'] = NODEFS;
-  this['IDBFS'] = IDBFS;
-  this['WORKERFS'] = WORKERFS;
 };
-if (typeof module !== 'undefined' && module !== null && module.exports) module.exports = Kawari;
+if (typeof module !== 'undefined' && module !== null && module.exports) module.exports = instantiate;
